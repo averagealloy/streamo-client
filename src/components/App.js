f38195d316addbb989bb2a62707c5fa1f2d5a08e
averagealloy <m.mctighe10@gmail.com>
2020-05-01 13:30:27 -0400

    dont do it this way

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,21 @@ import React from 'react'
 import { BrowserRouter, Route} from 'react-router-dom'
 
 const PageOne = () => {
-    return <div>PageOne</div>
+    return (
+    <div>
+        PageOne
+        <a href="/pagetwo">go to page two</a>
+    </div>
+    )
 }
+
 
 const PageTwo = () => {
     return(
         <div>
             PageTwo
             <button>click meh</button> 
+            <a href="/">go to page one</a>
         </div>
         ) 
 }
