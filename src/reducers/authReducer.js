f11c39cadbe7b327b73f77ad64f3a9d1289df55d
averagealloy <m.mctighe10@gmail.com>
2020-07-08 16:52:56 -0400

    giving case statement a value of isSignedIn to be t/f if user is signed in our out

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -6,8 +6,11 @@ export default (state = INTIAL_STATE, action) => {
 
     switch (action.type) {
         case 'SIGN_IN':
-
-        case 'SIGN_OUT'
+            return {...state, isSignedIn: true}
+        case 'SIGN_OUT':
+            return {...state, isSignedIn: false}
+        default:
+            return state;
     }
 
 };
