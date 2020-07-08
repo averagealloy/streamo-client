0b683c7cb45d49fae25a4726fb94e7fd65af82ad
averagealloy <m.mctighe10@gmail.com>
2020-07-08 16:50:44 -0400

    setting up case for sign in and out

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,13 @@
-export default (state = {}, action) => {
+const INTIAL_STATE = {
+    isSignedIn: null
+};
+
+export default (state = INTIAL_STATE, action) => {
+
+    switch (action.type) {
+        case 'SIGN_IN':
+
+        case 'SIGN_OUT'
+    }
 
 };
