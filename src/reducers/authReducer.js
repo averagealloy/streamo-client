10e838943ebdef699d9d66ba45587ffd492d377a
averagealloy <m.mctighe10@gmail.com>
2020-07-14 10:54:25 -0400

    replacing the sttings with the placeholders in types

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import {SIGN_IN, SIGN_OUT } from '../actions/types'
+
 const INTIAL_STATE = {
     isSignedIn: null
 };
@@ -5,9 +7,9 @@ const INTIAL_STATE = {
 export default (state = INTIAL_STATE, action) => {
 
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             return {...state, isSignedIn: true}
-        case 'SIGN_OUT':
+        case SIGN_OUT:
             return {...state, isSignedIn: false}
         default:
             return state;
