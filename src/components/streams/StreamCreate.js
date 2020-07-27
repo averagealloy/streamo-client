5a1c4c73d521c3469c54c219e2b596541fced68b
averagealloy <m.mctighe10@gmail.com>
2020-07-27 13:46:15 -0400

    have a submit button that console.logs the title and des via a submitbutton

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -13,12 +13,17 @@ renderInput({ input, label }){
 
 }
 
+onSubmit(formValues) {
+    console.log(formValues)
+}
+
 
     render() {
         return(
-            <form>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" component={this.renderInput} label="Enter Title"/>
                 <Field name="description" component={this.renderInput} label="Enter Description"/>
+                <button className="ui button primary">Submit</button>
             </form>
         )    
     
